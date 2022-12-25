239bff35f318c3e0a50f27296e2e201c2ab22cec
Lester Ong <lesterong776@gmail.com>
2022-12-25 11:28:40 +0800

    Serve build folder as index

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ morgan.token('body', req => {
   return JSON.stringify(req.body)
 })
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
+app.use(express.static('build'))
 
 let persons = [
     { 
